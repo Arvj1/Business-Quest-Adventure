2ad22f6b3a435321ef9dee10076efe9db0eadb25
Priyansh-Athena <priyansh.kumar@athenaeducation.co>
2025-03-20 12:10:52 +0530

    Added the Video Instruction Panel

---
+++ Assets/Scripts/InstructionHandler.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D.Aseprite;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class InstructionHandler : MonoBehaviour
+{
+    [SerializeField] VideoPlayer videoPlayer;
+    [SerializeField] Image playPauseBtn;
+    [SerializeField] Sprite playSprite, pauseSprite;
+    public UnityEvent OnVideoSkipButtonPressed, OnInstructionSkipButtonPressed;
+
+    private bool playToggle = false;
+   public void PlayButtonAction()
+   {
+        playToggle = !playToggle;
+        if (playToggle)
+        {
+            playPauseBtn.sprite = pauseSprite;
+            videoPlayer.Play();
+        }
+        else
+        {
+            playPauseBtn.sprite = playSprite;
+            videoPlayer.Pause();
+        }
+   }
+    
+    public void VideoSkipButtonAction()
+    {
+        OnVideoSkipButtonPressed.Invoke();
+    }
+
+    public void InstructionSkipButtonAction()
+    {
+        OnInstructionSkipButtonPressed.Invoke();
+    }
+}
